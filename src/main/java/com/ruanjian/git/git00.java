769e9adb31867df4bcbee2e116d231b3828d209a
黑Sir <fcrpl642@163.com>
2023-11-01 09:09:09 +0800

    M1

--- src/main/java/com/ruanjian/git/git00.java
+++ src/main/java/com/ruanjian/git/git00.java
@@ -3,8 +3,10 @@ package com.ruanjian.git;
 public class git00 {
     public static void main(String[] args) {
         System.out.println("hello git!");
-        System.out.println("hello git! 16666666666666");
+        System.out.println("hello git! 1");
         System.out.println("hello git! 2");
+        System.out.println("hello git! 3");
         System.out.println("hello git! 4");
+        System.out.println("master text");
     }
 }
