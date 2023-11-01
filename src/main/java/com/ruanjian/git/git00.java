f03f8cac0ddd5a5662c46d16300e6eed9a2be15f
黑Sir <fcrpl642@163.com>
2023-11-01 08:58:22 +0800

    commit 2

--- src/main/java/com/ruanjian/git/git00.java
+++ src/main/java/com/ruanjian/git/git00.java
@@ -3,7 +3,7 @@ package com.ruanjian.git;
 public class git00 {
     public static void main(String[] args) {
         System.out.println("hello git!");
-        System.out.println("hello git! 1");
+        System.out.println("hello git! 16666666666666");
         System.out.println("hello git! 2");
         System.out.println("hello git! 4");
     }
